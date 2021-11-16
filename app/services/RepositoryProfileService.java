702c85cdc802762b2181693a9b37899d6fd1d1a3
Sagar7421 <imsagar2922@gmail.com>
2021-11-16 13:04:12 -0500

    Added latest 20 issue list to repository details

--- app/services/RepositoryProfileService.java
+++ app/services/RepositoryProfileService.java
@@ -2,12 +2,15 @@ package services;
 
 
 import models.GithubClient;
+import models.RepoIssue;
 import models.RepositoryProfile;
 
 import javax.inject.Inject;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author Sagar Sanghani
@@ -21,6 +24,12 @@ public class RepositoryProfileService {
     public RepositoryProfileService(GithubClient github){
         this.sessions = new HashMap<>();
         this.github = github;
+    }
+
+    public CompletionStage<RepositoryProfile> getRepoDetails(String user, String repo) throws Exception {
+        List<RepoIssue> issueList = github.getLatestIssues(user, repo).toCompletableFuture().get();
+        CompletionStage<RepositoryProfile> repoProfile = github.getRepositoryDetails(user, repo, issueList);
+        return repoProfile;
     }
 
 }
